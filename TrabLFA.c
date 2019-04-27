f163acd1ee70a9dfc19b6d2849e9ec66091f3a65
Gabriel Borralho <39606678+gabrielrhcp@users.noreply.github.com>
2019-04-27 18:18:05 -0300

    melhora no nome das variaveis

--- TrabLFA.c
+++ TrabLFA.c
@@ -7,23 +7,23 @@
 // state 0 = 'x' ou 'z'
 // state 1 = 'x' -> 'x', 'z', 'v' ou final
 // state 2 = 'y' -> 'y' ou final
-// state 3 = 'z' -> 'z', 'v' ou final 
+// state 3 = 'z' -> 'z', 'v' ou final
 // state 4 = 'v' -> final
 // state 5 = final
 
-int teste(char *str, int aux, int state){
-    if(strlen(str) != aux){
-        
+int avaliar(char *str, int pos, int state){
+    if(strlen(str) != pos){
+
         // STATE 0
         if(state == 0){
-            if(str[aux] == 'x'){
-                aux++;
+            if(str[pos] == 'x'){
+                pos++;
                 state = 1;
-                teste(str, aux, state);
-            }else if(str[aux] == 'z'){
-                aux++;
+                avaliar(str, pos, state);
+            }else if(str[pos] == 'z'){
+                pos++;
                 state = 3;
-                teste(str, aux, state);
+                avaliar(str, pos, state);
             }else{
                 return false;
             }
@@ -31,21 +31,21 @@ int teste(char *str, int aux, int state){
 
         // STATE 1
         else if(state == 1){
-            if(str[aux] == 'x'){
-                aux++;
-                teste(str, aux, state);
-            }else if(str[aux] == 'y'){
-                aux++;
+            if(str[pos] == 'x'){
+                pos++;
+                avaliar(str, pos, state);
+            }else if(str[pos] == 'y'){
+                pos++;
                 state = 2;
-                teste(str, aux, state);
-            }else if(str[aux] == 'z'){
-                aux++;
+                avaliar(str, pos, state);
+            }else if(str[pos] == 'z'){
+                pos++;
                 state = 3;
-                teste(str, aux, state);
-            }else if(str[aux] == 'v'){
-                aux++;
+                avaliar(str, pos, state);
+            }else if(str[pos] == 'v'){
+                pos++;
                 state = 4;
-                teste(str, aux, state);
+                avaliar(str, pos, state);
             }else{
                 return false;
             }
@@ -53,9 +53,9 @@ int teste(char *str, int aux, int state){
 
         // STATE 2
         else if (state == 2){
-            if(str[aux] == 'y'){
-                aux++;
-                teste(str, aux, state);
+            if(str[pos] == 'y'){
+                pos++;
+                avaliar(str, pos, state);
             }else{
                 return false;
             }
@@ -63,13 +63,13 @@ int teste(char *str, int aux, int state){
 
         // STATE 3
         else if(state == 3){
-            if(str[aux] == 'z'){
-                aux++;
-                teste(str, aux, state);
-            }else if(str[aux] == 'v'){
-                aux++;
+            if(str[pos] == 'z'){
+                pos++;
+                avaliar(str, pos, state);
+            }else if(str[pos] == 'v'){
+                pos++;
                 state = 4;
-                teste(str, aux, state);
+                avaliar(str, pos, state);
             }else{
                 return false;
             }
@@ -77,9 +77,9 @@ int teste(char *str, int aux, int state){
 
         // STATE 4
         else if(state == 4){
-            return false;        
+            return false;
         }
-    
+
     // STATE 5
     }else{
         return true;
@@ -88,35 +88,34 @@ int teste(char *str, int aux, int state){
 
 int main(){
 
-    int ctrl;
+    int ctrl_menu; // variavel q controla o menu
 
     do{
         system("cls");
-        int aux = 0, state = 0;
-        char str[10]; char c;
-        int pos;
+        int pos = 0; // variavel q indica a posicao na string
+        int state = 0;  // variavavel q indica o estado da palavra
+        char str[10]; // variavel q recebe a palavra
+
         do{
             system("cls");
             printf("Digite uma palavra com no maximo 6 letras utilizando 'x', 'z', 'v' e 'y':\n");
             scanf("%s",&str);
         }while(strlen(str) > 6);
 
-        int result = teste(str, aux, state);
+        int result = avaliar(str, pos, state); // variavel q recebe o resultado da palavra
+
         if(result == true){
-            printf("\nPalvavra aceita pela linguagem.\n");
+            printf("\nPalvavra ACEITA pela Linguagem.\n");
         }else{
-            printf("\nNao e palvavra aceita pela linguagem\n");
+            printf("\nPalavra NAO ACEITA pela Linguagem\n");
         }
-        printf("\n");
-        
+
         //MENU
         do{
-            printf("[1]CONTINUAR\n[2]SAIR\nDIGITE: ");
-            scanf("%d",&ctrl);
-            printf("\n");
-        }while(ctrl != 1 && ctrl != 2);
+            printf("\n[1]CONTINUAR\n[2]SAIR\nDIGITE: ");
+            scanf("%d",&ctrl_menu);
+        }while(ctrl_menu != 1 && ctrl_menu != 2);
 
-    }while(ctrl == 1);
+    }while(ctrl_menu == 1);
 
 }
-
